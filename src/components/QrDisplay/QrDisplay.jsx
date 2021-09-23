2465fb513e7091de7b12abddaafb0d52e8c351fa
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-09-23 14:18:35 +0530

    feat: implement feature to download qr code - download either as png or svg - add margin to qr code

--- src/components/QrDisplay/QrDisplay.jsx
+++ src/components/QrDisplay/QrDisplay.jsx
@@ -1,14 +1,52 @@
 import { observer } from "mobx-react-lite"; // Or "mobx-react".
 import QRCode from "qrcode.react";
-
+import { svgAsPngUri, svgAsDataUri } from "save-svg-as-png";
 const QrDisplay = observer(({ userStore }) => {
+  const generateDownloadFromDataURL = (url, extension) => {
+    let downloadLink = document.createElement("a");
+    downloadLink.href = url;
+    downloadLink.download = "qrCode." + extension;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
+  const downloadQRAsPNG = async () => {
+    const canvas = document.getElementById("qrCode");
+    const options = {
+      scale: 10,
+    };
+    const pngUrl = await svgAsPngUri(canvas, options);
+    generateDownloadFromDataURL(pngUrl, "png");
+  };
+
+  const downloadQRAsSVG = async () => {
+    const canvas = document.getElementById("qrCode");
+    const options = {
+      scale: 10,
+    };
+    const svgUrl = await svgAsDataUri(canvas, options);
+    generateDownloadFromDataURL(svgUrl, "svg");
+  };
+
   return (
     <div>
-      <QRCode
-        className="border border-white"
-        value={userStore.linkUrl}
-        renderAs="svg"
-      />
+      <div className="p-3">
+        <QRCode
+          id="qrCode"
+          value={userStore.linkUrl}
+          renderAs="svg"
+          size="200"
+          includeMargin={true}
+        />
+      </div>
+      <h5>Download QR </h5>
+      <a className="m-2" href="# " onClick={downloadQRAsPNG}>
+        PNG
+      </a>
+      <a className="m-2" href="# " onClick={downloadQRAsSVG}>
+        SVG
+      </a>
     </div>
   );
 });
