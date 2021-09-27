d540f36a838b814d0feba41bd10532332c40436b
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-09-27 22:35:58 +0530

    feat: implement reach router for link routing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import "./App.css";
 import QrDisplay from "./components/QrDisplay/QrDisplay";
 import LinkInput from "./components/LinkInput/LinkInput";
 import Navigation from "./components/Navigation/Navigation";
+import TopMenuBar from "./components/TopMenuBar/TopMenuBar";
 import { observer } from "mobx-react-lite";
+import { Router } from "@reach/router";
 
 const App = observer(({ userStore }) => {
   return (
@@ -14,7 +16,11 @@ const App = observer(({ userStore }) => {
       }
     >
       <Navigation userStore={userStore} />
-      <LinkInput userStore={userStore} />
+      <TopMenuBar userStore={userStore} />
+      <Router>
+        <LinkInput userStore={userStore} path="/link" />
+        <LinkInput userStore={userStore} path="/" />
+      </Router>
       <QrDisplay userStore={userStore} />
     </div>
   );
